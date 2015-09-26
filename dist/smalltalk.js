da0acceb6d7675c737cc38c372fc8e50aabe5978
coderaiser <mnemonic.enemy@gmail.com>
2015-09-26 07:53:39 -0400

    chore(package) v1.3.4

--- dist/smalltalk.js
+++ dist/smalltalk.js
@@ -105,6 +105,7 @@
                 switch (keyCode) {
                     case KEY.ENTER:
                         closeDialog(el, dialog, ok, cancel);
+                        event.preventDefault();
                         break;
 
                     case KEY.ESC:
