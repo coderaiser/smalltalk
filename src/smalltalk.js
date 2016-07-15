42c14a5688c4c5f7244ceff9921f57a53429da1e
coderaiser <mnemonic.enemy@gmail.com>
2016-07-15 20:50:40 +0300

    feature(smalltalk) IIFE -> commonjs

--- src/smalltalk.js
+++ src/smalltalk.js
@@ -1,253 +1,240 @@
-(function(global) {
-    'use strict';
+'use strict';
+
+const remove            = bind(removeEl, '.smalltalk');
+
+const BUTTON_OK         = ['OK'];
+const BUTTON_OK_CANCEL  = ['OK', 'Cancel'];
     
-    if (typeof module !== 'undefined' && module.exports)
-        module.exports      = SmallTalk();
-    else
-        global.smalltalk    = SmallTalk();
+exports.alert = (title, msg, options) => {
+    return showDialog(title, msg, '', BUTTON_OK, options);
+};
+
+exports.prompt = (title, msg, value, options) => {
+    const val         = value || '';
+    const valueStr    = `<input type="text" value="${ val }" data-name="js-input">`;
     
-    function SmallTalk(callback) {
-        if (!new.target)
-            return new SmallTalk(callback);
+    return showDialog(title, msg, valueStr, BUTTON_OK_CANCEL, options);
+};
+
+exports.confirm = (title, msg, options) => {
+    return showDialog(title, msg, '', BUTTON_OK_CANCEL, options);
+};
+
+function getTemplate(title, msg, value, buttons) {
+    if (!Array.isArray(buttons))
+        throw Error('buttons should be array!');
+    
+    return `<div class="page">
+        <div data-name="js-close" class="close-button"></div>
+        <header>${ title }</header>
+        <div class="content-area">
+            ${ msg }
+            ${ value }
+        </div>
+        <div class="action-area">
+            <div class="button-strip"> ${
+                buttons.map((name, i) =>
+                    `<button tabindex=${ i } data-name="js-${ name.toLowerCase() }">${ name }</button>`
+                ).join('')
+            }
+            </div>
+        </div>
+    </div>`;
+}
+
+function showDialog(title, msg, value, buttons, options) {
+    let ok, cancel;
+    const dialog  = document.createElement('div'),
         
-        const remove            = bind(removeEl, '.smalltalk');
+        closeButtons    = [
+            'cancel',
+            'close',
+            'ok'
+        ],
         
-        const BUTTON_OK         = ['OK'];
-        const BUTTON_OK_CANCEL  = ['OK', 'Cancel'];
-        
-        this.alert = (title, msg, options) => {
-            return showDialog(title, msg, '', BUTTON_OK, options);
-        };
-        
-        this.prompt = (title, msg, value, options) => {
-            const val         = value || '';
-            const valueStr    = `<input type="text" value="${ val }" data-name="js-input">`;
+        promise = new Promise((resolve, reject) => {
+            const noCancel    = options && !options.cancel;
+            const empty       = () => {};
             
-            return showDialog(title, msg, valueStr, BUTTON_OK_CANCEL, options);
-        };
-        
-        this.confirm = (title, msg, options) => {
-            return showDialog(title, msg, '', BUTTON_OK_CANCEL, options);
-        };
-        
-        function getTemplate(title, msg, value, buttons) {
-            if (!Array.isArray(buttons))
-                throw Error('buttons should be array!');
+            ok      = resolve;
+            cancel  = reject;
             
-            return `<div class="page">
-                <div data-name="js-close" class="close-button"></div>
-                <header>${ title }</header>
-                <div class="content-area">
-                    ${ msg }
-                    ${ value }
-                </div>
-                <div class="action-area">
-                    <div class="button-strip"> ${
-                        buttons.map((name, i) =>
-                            `<button tabindex=${ i } data-name="js-${ name.toLowerCase() }">${ name }</button>`
-                        ).join('')
-                    }
-                    </div>
-                </div>
-            </div>`;
-        }
+            if (noCancel)
+                cancel = empty;
+        }),
         
-        function showDialog(title, msg, value, buttons, options) {
-            let ok, cancel;
-            const dialog  = document.createElement('div'),
-                
-                closeButtons    = [
-                    'cancel',
-                    'close',
-                    'ok'
-                ],
-                
-                promise = new Promise((resolve, reject) => {
-                    const noCancel    = options && !options.cancel;
-                    const empty       = () => {};
-                    
-                    ok      = resolve;
-                    cancel  = reject;
-                    
-                    if (noCancel)
-                        cancel = empty;
-                }),
-                
-                tmpl    = getTemplate(title, msg, value, buttons);
-                
-            dialog.innerHTML = tmpl;
-            dialog.className = 'smalltalk';
-            
-            document.body.appendChild(dialog);
-            
+        tmpl    = getTemplate(title, msg, value, buttons);
+        
+    dialog.innerHTML = tmpl;
+    dialog.className = 'smalltalk';
+    
+    document.body.appendChild(dialog);
+    
+    find(dialog, ['ok', 'input']).forEach(el =>
+        el.focus()
+    );
+    
+    find(dialog, ['input']).forEach(el => {
+        el.setSelectionRange(0, value.length);
+    });
+    
+    addListeterAll('click', dialog, closeButtons, event =>
+        closeDialog(event.target, dialog, ok, cancel)
+    );
+    
+    ['click', 'contextmenu'].forEach(event =>
+        dialog.addEventListener(event, () =>
             find(dialog, ['ok', 'input']).forEach(el =>
                 el.focus()
-            );
+            )
+        )
+    );
+    
+    dialog.addEventListener('keydown', keyDown(dialog, ok, cancel));
+    
+    return promise;
+}
+
+function keyDown(dialog, ok, cancel) {
+    return event => {
+        const KEY   = {
+            ENTER : 13,
+            ESC   : 27,
+            TAB   : 9,
+            LEFT  : 37,
+            UP    : 38,
+            RIGHT : 39,
+            DOWN  : 40
+        };
+        
+        const keyCode     = event.keyCode;
+        const el          = event.target;
+        
+        const namesAll    = ['ok', 'cancel', 'input'];
+        const names       = find(dialog, namesAll).map((el) => {
+            return getDataName(el);
+        });
+        
+        switch(keyCode) {
+        case KEY.ENTER:
+            closeDialog(el, dialog, ok, cancel);
+            event.preventDefault();
+            break;
+        
+        case KEY.ESC:
+            remove();
+            cancel();
+            break;
+        
+        case KEY.TAB:
+            if (event.shiftKey)
+                tab(dialog, names);
             
-            find(dialog, ['input']).forEach(el => {
-                el.setSelectionRange(0, value.length);
+            tab(dialog, names);
+            event.preventDefault();
+            break;
+        
+        default:
+            const is = ['left', 'right', 'up', 'down'].some((name) => {
+                return keyCode === KEY[name.toUpperCase()];
             });
             
-            addListeterAll('click', dialog, closeButtons, event =>
-                closeDialog(event.target, dialog, ok, cancel)
-            );
+            if (is)
+                changeButtonFocus(dialog, names);
             
-            ['click', 'contextmenu'].forEach(event =>
-                dialog.addEventListener(event, () =>
-                    find(dialog, ['ok', 'input']).forEach(el =>
-                        el.focus()
-                    )
-                )
-            );
-            
-            dialog.addEventListener('keydown', keyDown(dialog, ok, cancel));
-            
-            return promise;
+            break;
         }
         
-        function keyDown(dialog, ok, cancel) {
-            return event => {
-                const KEY   = {
-                    ENTER : 13,
-                    ESC   : 27,
-                    TAB   : 9,
-                    LEFT  : 37,
-                    UP    : 38,
-                    RIGHT : 39,
-                    DOWN  : 40
-                };
-                
-                const keyCode     = event.keyCode;
-                const el          = event.target;
-                
-                const namesAll    = ['ok', 'cancel', 'input'];
-                const names       = find(dialog, namesAll).map((el) => {
-                    return getDataName(el);
-                });
-                
-                switch(keyCode) {
-                case KEY.ENTER:
-                    closeDialog(el, dialog, ok, cancel);
-                    event.preventDefault();
-                    break;
-                
-                case KEY.ESC:
-                    remove();
-                    cancel();
-                    break;
-                
-                case KEY.TAB:
-                    if (event.shiftKey)
-                        tab(dialog, names);
-                    
-                    tab(dialog, names);
-                    event.preventDefault();
-                    break;
-                
-                default:
-                    const is = ['left', 'right', 'up', 'down'].some((name) => {
-                        return keyCode === KEY[name.toUpperCase()];
-                    });
-                    
-                    if (is)
-                        changeButtonFocus(dialog, names);
-                    
-                    break;
-                }
-                
-                event.stopPropagation();
-            };
-        }
-        
-        function getDataName(el) {
-            return el
-                .getAttribute('data-name')
-                .replace('js-', '');
-        }
-        
-        function changeButtonFocus(dialog, names) {
-            let name        = '';
-            const active    = document.activeElement,
-                activeName  = getDataName(active),
-                isButton    = /ok|cancel/.test(activeName),
-                count       = names.length - 1;
+        event.stopPropagation();
+    };
+}
+
+function getDataName(el) {
+    return el
+        .getAttribute('data-name')
+        .replace('js-', '');
+}
+
+function changeButtonFocus(dialog, names) {
+    let name        = '';
+    const active    = document.activeElement,
+        activeName  = getDataName(active),
+        isButton    = /ok|cancel/.test(activeName),
+        count       = names.length - 1;
+    
+    if (activeName !== 'input' && count && isButton) {
+        if (activeName === 'cancel')
+            name = 'ok';
+        else
+            name = 'cancel';
             
-            if (activeName !== 'input' && count && isButton) {
-                if (activeName === 'cancel')
-                    name = 'ok';
-                else
-                    name = 'cancel';
-                    
-                find(dialog, [name]).forEach(el => {
-                    el.focus();
-                });
-            }
-        }
+        find(dialog, [name]).forEach(el => {
+            el.focus();
+        });
+    }
+}
+
+function tab(dialog, names) {
+    const active    = document.activeElement,
+        activeName  = getDataName(active),
         
-        function tab(dialog, names) {
-            const active    = document.activeElement,
-                activeName  = getDataName(active),
-                
-                count       = names.length - 1;
-            let index       = names.indexOf(activeName);
-            
-            if (index === count)
-                index = 0;
-            else if (index < count)
-                ++index;
-            
-            const name = names[index];
-            
-            find(dialog, [name]).forEach(el =>
-                el.focus()
-            );
-        }
+        count       = names.length - 1;
+    let index       = names.indexOf(activeName);
+    
+    if (index === count)
+        index = 0;
+    else if (index < count)
+        ++index;
+    
+    const name = names[index];
+    
+    find(dialog, [name]).forEach(el =>
+        el.focus()
+    );
+}
+
+function closeDialog(el, dialog, ok, cancel) {
+    let value;
+    const name = el
+            .getAttribute('data-name')
+            .replace('js-', '');
+    
+    if (/close|cancel/.test(name)) {
+        cancel();
+    } else {
+        value = find(dialog, ['input']).reduce((value, el) => {
+            return el.value;
+        }, null);
         
-        function closeDialog(el, dialog, ok, cancel) {
-            let value;
-            const name = el
-                    .getAttribute('data-name')
-                    .replace('js-', '');
-            
-            if (/close|cancel/.test(name)) {
-                cancel();
-            } else {
-                value = find(dialog, ['input']).reduce((value, el) => {
-                    return el.value;
-                }, null);
-                
-                ok(value);
-            }
-            
-            remove();
-        }
-        
-        function find(element, names) {
-            const elements = names.map(name =>
-                element.querySelector(`[data-name="js-${ name }"]`)
-            ).filter(el =>
-                el
-            );
-            
-            return elements;
-        }
-        
-        function addListeterAll(event, parent, elements, fn) {
-            find(parent, elements).forEach(el =>
-                el.addEventListener(event, fn)
-            );
-        }
-        
-        function removeEl(name) {
-            var el = document.querySelector(name);
-            
-            el.parentElement.removeChild(el);
-        }
-        
-        function bind(fn, ...args) {
-            return () => fn(...args);
-        }
+        ok(value);
     }
     
-})(typeof window !== 'undefined' && window);
+    remove();
+}
+
+function find(element, names) {
+    const elements = names.map(name =>
+        element.querySelector(`[data-name="js-${ name }"]`)
+    ).filter(el =>
+        el
+    );
+    
+    return elements;
+}
+
+function addListeterAll(event, parent, elements, fn) {
+    find(parent, elements).forEach(el =>
+        el.addEventListener(event, fn)
+    );
+}
+
+function removeEl(name) {
+    var el = document.querySelector(name);
+    
+    el.parentElement.removeChild(el);
+}
+
+function bind(fn, ...args) {
+    return () => fn(...args);
+}
 
--- src/smalltalk.native.js
+++ src/smalltalk.native.js
@@ -1,54 +1,41 @@
-(function(global) {
-    'use strict';
+'use strict';
+
+exports.alert          = (title, message) => {
+    const promise = new Promise(function(resolve) {
+        alert(message);
+        resolve();
+    });
     
-    if (typeof module !== 'undefined' && module.exports)
-        module.exports = Smalltalk();
-    else
-        global.smalltalk = Smalltalk();
-        
+    return promise;
+};
+
+exports.prompt         = (title, message, value, options) => {
+    const o     = options,
+        promise = new Promise(function(resolve, reject) {
+            const noCancel = o && !o.cancel,
+                result  = prompt(message, value);
+            
+            if (result !== null)
+                resolve(result);
+            else if (!noCancel)
+                reject();
+        });
     
-    function Smalltalk() {
-        if (!(this instanceof Smalltalk))
-            return new Smalltalk();
-        
-        this.alert          = (title, message) => {
-            let promise = new Promise(function(resolve) {
-                alert(message);
+    return promise;
+};
+
+exports.confirm         = (title, message, options) => {
+    const o         = options,
+        noCancel    = o && !o.noCancel,
+        promise     = new Promise(function(resolve, reject) {
+            const is = confirm(message);
+            
+            if (is)
                 resolve();
-            });
-            
-            return promise;
-        };
-        
-        this.prompt         = (title, message, value, options) => {
-            let o       = options,
-                promise = new Promise(function(resolve, reject) {
-                    let noCancel = o && !o.cancel,
-                        result  = prompt(message, value);
-                    
-                    if (result !== null)
-                        resolve(result);
-                    else if (!noCancel)
-                        reject();
-                });
-            
-            return promise;
-        };
-        
-        this.confirm         = (title, message, options) => {
-            let o           = options,
-                noCancel    = o && !o.noCancel,
-                promise     = new Promise(function(resolve, reject) {
-                    let is = confirm(message);
-                    
-                    if (is)
-                        resolve();
-                    else if (!noCancel)
-                        reject();
-                });
-            
-            return promise;
-        };
-    }
+            else if (!noCancel)
+                reject();
+        });
     
-})(typeof window !== 'undefined' && window);
+    return promise;
+};
+
