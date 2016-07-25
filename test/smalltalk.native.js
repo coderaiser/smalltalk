b1ae35680fdc5eea614e606254ea5e3f630231bd
coderaiser <mnemonic.enemy@gmail.com>
2016-07-25 20:50:14 +0300

    test(smalltalk) add

---
+++ test/smalltalk.native.js
@@ -0,0 +1,127 @@
+'use strict';
+
+const test = require('tape');
+const sinon = require('sinon');
+
+const smalltalk = require('../lib/smalltalk.native');
+
+test('smalltalk.native: alert', (t) => {
+    const alert = sinon.stub();
+    global.alert = alert;
+    
+    smalltalk.alert('title', 'message');
+    t.ok(alert.calledWith('message'), 'alert should have been called with message');
+    t.end();
+});
+
+test('smalltalk.native: alert: result', (t) => {
+    const alert = sinon.stub();
+    global.alert = alert;
+    
+    smalltalk.alert('title', 'message').then(() => {
+        t.pass('promise should have been resolved');
+        t.end();
+    }).catch((e) => {
+        t.fail(`should not reject ${e.message}`);
+        t.end();
+    });
+});
+
+test('smalltalk.native: confirm', (t) => {
+    const confirm = sinon.stub();
+    global.confirm = confirm;
+    
+    smalltalk.confirm('title', 'message');
+    t.ok(confirm.calledWith('message'), 'confirm should have been called with message');
+    t.end();
+});
+
+test('smalltalk.native: confirm: result: ok', (t) => {
+    const confirm = sinon.stub().returns(true);
+    global.confirm = confirm;
+    
+    smalltalk.confirm('title', 'message').then(() => {
+        t.pass('should resolve');
+        t.end();
+    }).catch((e) => {
+        t.notOk(e, 'should reject');
+        t.end();
+    });
+});
+
+test('smalltalk.native: confirm: result: cancel', (t) => {
+    const confirm = sinon.stub().returns(false);
+    global.confirm = confirm;
+    
+    smalltalk.confirm('title', 'message').then(() => {
+        t.fail('should not resolve');
+        t.end();
+    }).catch((e) => {
+        t.notOk(e, 'should reject');
+        t.end();
+    });
+});
+
+test('smalltalk.native: confirm: options: cancel', (t) => {
+    const confirm = sinon.stub().returns(false);
+    global.confirm = confirm;
+    
+    smalltalk.confirm('title', 'message', {cancel: false}).then(() => {
+        t.pass('should resolve');
+        t.end();
+    }).catch(() => {
+        t.fail('should not reject');
+        t.end();
+    });
+});
+
+test('smalltalk.native: prompt', (t) => {
+    const prompt = sinon.stub();
+    global.prompt = prompt;
+    
+    smalltalk.prompt('title', 'message', 'value');
+    t.ok(prompt.calledWith('message', 'value'), 'prompt should have been called with message');
+    t.end();
+});
+
+test('smalltalk.native: prompt: result: ok', (t) => {
+    const prompt = sinon.stub().returns('hello');
+    global.prompt = prompt;
+    
+    smalltalk.prompt('title', 'message', 'value').then((value) => {
+        t.equal(value, 'hello', 'should resolve value');
+        t.end();
+    }).catch((e) => {
+        t.fail(`should not reject ${e.message}`);
+        t.end();
+    });
+});
+
+test('smalltalk.native: prompt: result: cancel', (t) => {
+    const prompt = sinon.stub().returns(null);
+    global.prompt = prompt;
+    
+    smalltalk.prompt('title', 'message', 'value').then(() => {
+        t.fail('should not resolve');
+        t.end();
+    }).catch((e) => {
+        t.notOk(e, 'should reject');
+        t.end();
+    });
+});
+
+test('smalltalk.native: prompt: options: cancel', (t) => {
+    const prompt = sinon.stub().returns();
+    global.prompt = prompt;
+    
+    smalltalk.prompt('title', 'message', 'value', {
+        cancel: false
+    }).then(() => {
+        t.pass('should resolve');
+        t.end();
+    }).catch((e) => {
+        t.fail(`should not reject ${e.message}`);
+        t.end();
+    });
+});
+
