785a10443edcafbd444035f9ccf487b53a681139
coderaiser <coderaiser@cloudcmd.io>
2017-02-23 13:47:13 +0200

    test(smalltalk) alert: fixture

--- test/smalltalk.js
+++ test/smalltalk.js
@@ -1,28 +1,86 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
+
 const test = require('tape');
 const sinon = require('sinon');
 
 const smalltalk = require('..');
+const fixtureDir = path.join(__dirname, 'fixture');
+const fixture = {
+    alert: fs.readFileSync(`${fixtureDir}/alert.html`, 'utf8')
+};
 
-test('smalltalk: alert', (t) => {
+test('smalltalk: alert: innerHTML', (t) => {
     before();
     
-    const createElement = getCreateElement();
+    const el = {
+        innerHTML: ''
+    };
+    
+    const createElement = getCreateElement(el);
     global.document.createElement = createElement;
     
     smalltalk.alert('title', 'message')
-    t.ok(createElement.calledWith('div'), 'alert should have been called with message');
+    t.equal(fixture.alert, el.innerHTML, 'should be equal');
     
     after();
     t.end();
 });
 
-function getCreateElement() {
-    return sinon.stub().returns({
-        querySelector: sinon.stub(),
+test('smalltalk: alert: appendChild', (t) => {
+    before();
+    
+    const el = {
+    };
+    
+    const createElement = getCreateElement(el);
+    global.document.createElement = createElement;
+    
+    smalltalk.alert('title', 'message')
+    t.ok(document.body.appendChild.calledWith(el), 'should append el');
+    
+    after();
+    t.end();
+});
+
+test('smalltalk: alert: querySelector', (t) => {
+    before();
+    
+    const el = {
+        querySelector: (a) => {
+            if (a === '[data-name="js-ok"]')
+                return ok;
+        }
+    };
+    
+    const ok = {
+        focus: sinon.stub(),
         addEventListener: sinon.stub(),
-    })
+    };
+    
+    const createElement = getCreateElement(el);
+    global.document.createElement = createElement;
+    
+    smalltalk.alert('title', 'message')
+    t.equal(ok.addEventListener.args.pop()[0], 'click', 'should');
+    
+    after();
+    t.end();
+});
+
+function getCreateElement(el = {}) {
+    const querySelector = sinon.stub();
+    const addEventListener = sinon.stub();
+    
+    if (!el.querySelector)
+        el.querySelector = querySelector;
+    
+    if (!el.addEventListener)
+        el.addEventListener = addEventListener;
+
+    return sinon.stub().returns(el);
 }
 
 function before() {
@@ -37,3 +95,4 @@ function before() {
 function after() {
     delete global.document;
 }
+
