eebebc1e21e9c47d876176548d6ffa65945f457e
coderaiser <coderaiser@cloudcmd.io>
2017-02-23 14:02:55 +0200

    test(smalltalk) alert: close

--- test/smalltalk.js
+++ test/smalltalk.js
@@ -45,7 +45,7 @@ test('smalltalk: alert: appendChild', (t) => {
     t.end();
 });
 
-test('smalltalk: alert: querySelector', (t) => {
+test('smalltalk: alert: click', (t) => {
     before();
     
     const el = {
@@ -64,12 +64,91 @@ test('smalltalk: alert: querySelector', (t) => {
     global.document.createElement = createElement;
     
     smalltalk.alert('title', 'message')
-    t.equal(ok.addEventListener.args.pop()[0], 'click', 'should');
+    t.equal(ok.addEventListener.args.pop()[0], 'click', 'should set click listener');
     
     after();
     t.end();
 });
 
+test('smalltalk: alert: close: querySelector', (t) => {
+    before();
+    
+    const el = {
+        parentElement: {
+            removeChild: () => {}
+        },
+        querySelector: (a) => {
+            if (a === '[data-name="js-ok"]')
+                return ok;
+        }
+    };
+    
+    const ok = {
+        getAttribute: () => 'js-ok',
+        focus: sinon.stub(),
+        addEventListener: sinon.stub(),
+    };
+    
+    const createElement = getCreateElement(el);
+    document.createElement = createElement;
+    
+    const querySelector = sinon.stub().returns(el);
+    document.querySelector = querySelector;
+    
+    smalltalk.alert('title', 'message')
+    
+    const [, close] = ok.addEventListener.args.pop();
+    
+    close({
+        target: ok
+    });
+    
+    t.equal(querySelector.args.pop().pop(), '.smalltalk', 'should find smalltalk');
+    
+    after();
+    t.end();
+});
+
+test('smalltalk: alert: close: remove', (t) => {
+    before();
+    
+    const parentElement = {
+        removeChild: sinon.stub()
+    };
+    
+    const el = {
+        parentElement,
+        querySelector: (a) => {
+            if (a === '[data-name="js-ok"]')
+                return ok;
+        }
+    };
+    
+    const ok = {
+        getAttribute: () => 'js-ok',
+        focus: sinon.stub(),
+        addEventListener: sinon.stub(),
+    };
+    
+    const createElement = getCreateElement(el);
+    document.createElement = createElement;
+    
+    const querySelector = sinon.stub().returns(el);
+    document.querySelector = querySelector;
+    
+    smalltalk.alert('title', 'message')
+    
+    const [, close] = ok.addEventListener.args.pop();
+    
+    close({
+        target: ok
+    });
+    
+    t.equal(parentElement.removeChild.args.pop().pop(), el, 'should find smalltalk');
+    
+    after();
+    t.end();
+});
 function getCreateElement(el = {}) {
     const querySelector = sinon.stub();
     const addEventListener = sinon.stub();
